c2b00da0e116e4bdedbc57a4e44155dbb97b39b9
Suprav Kandel <dynosups@gmail.com>
2021-05-21 13:15:21 +0545

    Adding initial files

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(("Title"), max_length=200)
+    text = models.TextField(("Content"))
+    # image = models.ImageField(("Image"), upload_to="post/images", null=True, blank=True)
+    created_date = models.DateTimeField(('Created Date'), auto_now_add=False)
+    published_date = models.DateTimeField(("Published Date"), auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
